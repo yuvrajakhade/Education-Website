7645cd3aea3eb092ff93fc691ed42466edf6cdb6
yuvraj <yuvraj.akhade399.com>
2022-10-09 17:47:40 +0530

    Added educational website

---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Heading from "../../common/heading/Heading";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <>
+      <section className="hero">
+        <div className="container">
+          <div className="row">
+            <Heading
+              subtitle="WELCOME TO KUDALE COACHING CLASSES"
+              title="Best Education Expertise"
+            />
+            <p>
+              A Great Place To Learn Learning Today For A Better Tomorrow Be a
+              reader, be a writer, be a problem solver
+            </p>
+            <div className="button">
+              <button className="primary-btn">
+                GET STARTED NOW <i className="fa fa-long-arrow-alt-right"></i>
+              </button>
+              <button>
+                VIEW COURSE <i className="fa fa-long-arrow-alt-right"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="margin"></div>
+    </>
+  );
+};
+
+export default Hero;
